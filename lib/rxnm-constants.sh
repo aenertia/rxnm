9699360c24a059834f50d74196f7232ca26fa325
Joel Wirāmu Pauling <aenertia@aenertia.net>
2026-02-22 10:55:23 +1300

    chore: finalize v1.1.0 documentation and version strings

--- lib/rxnm-constants.sh
+++ lib/rxnm-constants.sh
@@ -15,7 +15,7 @@
 # -----------------------------------------------------------------------------
 
 # --- Identity & Defaults ---
-: "${RXNM_VERSION:=1.1.0-rc3}"
+: "${RXNM_VERSION:=1.1.0}"
 : "${RXNM_API_VERSION:=1.1}"
 : "${DEFAULT_HOSTNAME:=ROCKNIX}"
 
